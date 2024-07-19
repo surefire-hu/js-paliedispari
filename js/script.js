af5865f9677f45db6798134bf4aee38d61881434
surefire-hu <surefire.hu2001@gmail.com>
2024-07-19 20:51:32 +0200

    Palindromo con il ciclo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+function palindrome(word) {
+    
+    // Usa un ciclo for per confrontare i caratteri
+    for (let i = 0; i < (word.length / 2); i++) {
+        if (word[i] !== word[word.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
+// Chiedi all'utente di inserire una parola
+let userWord = prompt("Inserisci una parola per verificare se è palindroma:");
+
+// Risultato
+if (palindrome(userWord)) {
+    console.log(`"${userWord}" è palindroma!`);
+} else {
+    console.log(`"${userWord}" non è palindroma.`);
+}
